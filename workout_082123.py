869f0a00d183181fff96ddb4a489422f9de5e9e2
Austin Rippee <austinrippee@gmail.com>
2023-08-24 13:05:20 -0400

    Add files via upload

--- workout_082123.py
+++ workout_082123.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import math
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("workout_082123.csv")
 
@@ -11,9 +12,9 @@ def time_str_to_seconds(time_str):
             total_seconds = int(minutes) * 60 + float(seconds)
             return total_seconds
         else:
-            return None  # Handle cases where time format is not valid
+            return 0  # Handle cases where time format is not valid
     except ValueError:
-        return None  # Handle invalid time format gracefully
+        return 0  # Handle invalid time format gracefully
     
 def seconds_to_time_str(total_seconds):
     if math.isnan(total_seconds):
@@ -31,7 +32,12 @@ def convert_to_float_or_none(value):
     except ValueError:
         return None
 
-df['2mi_in_sec'] = df['2mi'].apply(time_str_to_seconds)
+
+# Clean and convert '2mi' column
+df['2mi'] = df['2mi'].apply(lambda x: x if ':' in str(x) else '0:00')
+df['2mi'] = df['2mi'].apply(time_str_to_seconds)
+
+df['2mi_in_sec'] = df['2mi']
 df['800.1_in_sec'] = df['800'].apply(time_str_to_seconds)
 df['800.2_in_sec'] = df['800.1'].apply(time_str_to_seconds)
 df['800.3_in_sec'] = df['800.2'].apply(time_str_to_seconds)
@@ -47,6 +53,21 @@ df['8toMi'] = df['8toMi'].apply(seconds_to_time_str)
 df['800avg'] = df['800avg'].apply(seconds_to_time_str)
 df['3miEst'] = df['3miEst'].apply(seconds_to_time_str)
 df['5kEst'] = df['5kEst'].apply(seconds_to_time_str)
+
+# Sort the DataFrame by '2mi' in ascending order and '800avg' in descending order
+df = df.sort_values(by=['2mi', '800avg'], ascending=[True, True])
+
+ax1 = df.plot.scatter(x='2mi', y='800avg', c='DarkBlue')
+
+# Get the x and y limits of the plot
+x_min, x_max = ax1.get_xlim()
+y_min, y_max = ax1.get_ylim()
+
+# Reverse the x-axis and y-axis limits to achieve the desired sorting
+ax1.set_xlim(x_max, x_min)  # Reverse x-axis
+ax1.set_ylim(y_max, y_min)  # Reverse y-axis
+
+plt.show()
 
 #df1 = df[['Name', '2mi']].sort_values(['2mi'], ascending=True)
 
